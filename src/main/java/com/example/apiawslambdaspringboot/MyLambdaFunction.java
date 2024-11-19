f9b1ce8d77fda321f3da0f05b8141c5deba4bbec
amfreitas1981 <32101414+amfreitas1981@users.noreply.github.com>
2024-11-19 10:40:53 -0300

    Update MyLambdaFunction.java

--- src/main/java/com/example/apiawslambdaspringboot/MyLambdaFunction.java
+++ src/main/java/com/example/apiawslambdaspringboot/MyLambdaFunction.java
@@ -15,7 +15,22 @@ public class MyLambdaFunction implements RequestHandler<Object, Object> {
     private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
+        // Inicia o contexto do Spring
         applicationContext = SpringApplication.run(MyLambdaFunction.class, args);
+
+        // Imprime o valor da variavel applicationContext
+        System.out.println("Valor Variável: " + applicationContext);
+
+        // Cria uma instancia de MyLambdaFunction
+        MyLambdaFunction lambdaFunction = new MyLambdaFunction();
+
+        // Simula os argumentos para handleRequest
+        Object input = "Teste de entrada"; // Simule aqui o objeto de entrada
+        Context context = new MockContext(); // Use uma classe mock para simular o contexto
+
+        // Chama o metodo handleRequest e imprime o retorno
+        Object retorno = lambdaFunction.handleRequest(input, context);
+        System.out.println("Retorno método 'handleRequest': " + retorno);
     }
 
     @Override
