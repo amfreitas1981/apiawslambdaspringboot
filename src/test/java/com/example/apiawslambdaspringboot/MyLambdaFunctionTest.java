780b3c3df7812040cb7900beb8836c020fb85c13
Alexandre Freitas <alexandremfreitas@gmail.com>
2025-01-16 11:44:41 -0300

    Update import, dependencies and test classes.

--- src/test/java/com/example/apiawslambdaspringboot/MyLambdaFunctionTest.java
+++ src/test/java/com/example/apiawslambdaspringboot/MyLambdaFunctionTest.java
@@ -1,74 +1,84 @@
 package com.example.apiawslambdaspringboot;
-//
-//import com.amazonaws.services.lambda.runtime.Context;
-//import com.amazonaws.services.lambda.runtime.LambdaLogger;
-//import com.example.apiawslambdaspringboot.controller.MyController;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.ResponseEntity;
-//
-//import org.springframework.context.ApplicationContext;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.mockStatic;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest(classes = MyLambdaFunction.class, properties = "server.port=8080")
-//@ExtendWith(MockitoExtension.class)
-//class MyLambdaFunctionTest {
-//
-//    @InjectMocks
-//    private MyLambdaFunction lambdaFunction;
-//
-//    @MockBean
-//    private ApplicationContext applicationContext;
-//
-//    @Mock
-//    private MyController myController;
-//
-//    @Mock
-//    private Context context;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MyLambdaFunction.applicationContext = applicationContext;
-//        when(applicationContext.getBean(MyController.class)).thenReturn(myController);
-//        when(context.getLogger()).thenReturn(Mockito.mock(LambdaLogger.class));
-//    }
-//
-//    @Test
-//    void testHandleRequest() {
-//        Object input = "Test Input";
-//        ResponseEntity<String> mockedResponse = ResponseEntity.ok("Mocked Response");
-//
-//        when(myController.handleRequest(input)).thenReturn(mockedResponse);
-//
-//        ResponseEntity<String> result = (ResponseEntity<String>) lambdaFunction.handleRequest(input, context);
-//
-//        verify(context.getLogger()).log(Mockito.contains("Input: Test Input"));
-//        verify(myController).handleRequest(input);
-//        assertEquals("Mocked Response", result.getBody());
-//    }
-//
-//    @Test
-//    void testMain() {
-//        try (var mockedSpringApplication = mockStatic(SpringApplication.class)) {
-//            mockedSpringApplication
-//                    .when(() -> SpringApplication.run(MyLambdaFunction.class, new String[]{}))
-//                    .thenReturn(null);
-//
-//            MyLambdaFunction.main(new String[]{});
-//
-//            mockedSpringApplication.verify(() -> SpringApplication.run(MyLambdaFunction.class, new String[]{}));
-//        }
-//    }
-//}
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.example.apiawslambdaspringboot.context.MockContext;
+import com.example.apiawslambdaspringboot.controller.MyController;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class MyLambdaFunctionTest {
+
+    @InjectMocks
+    private MyLambdaFunction lambdaFunction;
+
+    @Mock
+    private MyController myController;
+
+    @Mock
+    private ApplicationContext applicationContext;
+
+    private Context context;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        context = new MockContext();
+        // Simula o ApplicationContext
+        lambdaFunction.applicationContext = applicationContext;
+        lenient().when(applicationContext.getBean(MyController.class)).thenReturn(myController);
+        // Redireciona a saída do console para capturar a saída do método main
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        // Restaura a saída padrão do console
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testHandleRequest() {
+        // Define o comportamento esperado do método handleRequest do MyController
+        when(myController.handleRequest("Teste de entrada"))
+                .thenReturn(ResponseEntity.ok("Hello, AWS Lambda! Server port: 8080"));
+
+        // Chama o método handleRequest da MyLambdaFunction
+        ResponseEntity<String> response = (ResponseEntity<String>) lambdaFunction.handleRequest("Teste de entrada", context);
+
+        // Verifica se o retorno está correto
+        assertEquals("Hello, AWS Lambda! Server port: 8080", response.getBody());
+    }
+
+    @Test
+    public void testMainMethod() {
+        // Simula os argumentos para o método main
+        String[] args = new String[]{};
+
+        // Executa o método main
+        MyLambdaFunction.main(args);
+
+        // Verifica a saída do console
+        String output = outContent.toString();
+        assertTrue(output.contains("Valor Variável:"));
+        assertTrue(output.contains("Retorno método 'handleRequest':"));
+    }
+}
